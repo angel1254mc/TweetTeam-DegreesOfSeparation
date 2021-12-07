fdf1aec7ded370e7e6f42e2b0d1c20bd641d9a35
JeyaI <33754570+JeyaI@users.noreply.github.com>
2021-12-06 20:35:41 -0500

    Update djikstra.cpp

--- src/djikstra.cpp
+++ src/djikstra.cpp
@@ -16,16 +16,22 @@ double AdjList::djikstra(int from, int to){
 	*/
 	priority_queue < pair<double, int>, vector< pair<double, int> >, greater< pair<double, int> > > pqueue; //(weight, vertex)
 	
-	vector<double> distances(this->total_size, INT_MAX);
+	vector<double> distances(this->curr_size+1, INT_MAX);
 	//vector<int> previous(this->total_size, -1);
 	
 	pqueue.push(pair<double, int>{0.0, from});
 	
 	distances[from] = 0.0;
+	int count = 0;
 	
-	while(!queue.empty()){
-		int vertex = queue.top().second;
+	while(!pqueue.empty()){
+		int vertex = pqueue.top().second;
 		pqueue.pop();
+		
+		count++;
+		if(count%1000 == 0){
+			cout << "1k processed";
+		}
 		
 		auto adjacentVertices = this->getAdjacent(vertex);
 		for(auto adjacent : adjacentVertices){
@@ -41,38 +47,41 @@ double AdjList::djikstra(int from, int to){
 	return distances[to];
 }
 
-//TODO
 double EdgeList::djikstra(int from, int to){
 	/*
 	Notes: 
 		Current implementation does not require "previous" vector.
 		Returns the shortest path between two nodes.	
 	*/
-	priority_queue < pair<double, int>, vector< pair<double, int> >, greater< pair<double, int> > > queue; //(weight, vertex)
+	priority_queue < pair<double, int>, vector< pair<double, int> >, greater< pair<double, int> > > pqueue; //(weight, vertex)
 	int vertices_count = this->getNumVertices();
-	vector<double> distances(vertices_count, INT_MAX);
+	vector<double> distances(vertices_count + 1, INT_MAX);
 	//vector<int> previous(this->total_size, -1);
 	
-	queue.push(pair<double, int>{0.0, from});
+	pqueue.push(pair<double, int>{0.0, from});
 	
 	distances[from] = 0.0;
-	
-	while(!queue.empty()){
-		int vertex = queue.top().second;
-		queue.pop();
-		
-		adjacentVertices = this->getAdjacent(vertex);
+	int count = 0;
+	while(!pqueue.empty()){
+		int vertex = pqueue.top().second;
+		pqueue.pop();
+		count++;
+		if(count%1000 == 0){
+			cout << "1k processed";
+		}
+		//cout << pqueue.size() << endl;
+		auto adjacentVertices = this->getAdjacent(vertex);
 		for(auto adjacent : adjacentVertices){
 			//USING PSEUDOCODE FROM AMAN'S LECTURE ON GRAPH ALGORITHMS FOR THIS RELAXATION//
 			if(distances[vertex] + this->getWeight(vertex, adjacent) < distances[adjacent]){
 				distances[adjacent] = distances[vertex] + this->getWeight(vertex, adjacent);
 				//previous[adjacent] = vertex;
-				queue.push(pair<double, int>{distances[adjacent], adjacent});
+				pqueue.push(pair<double, int>{distances[adjacent], adjacent});
 			}
 			
 		}
 	}
 	
 	return distances[to];
-}
+}    
 	
