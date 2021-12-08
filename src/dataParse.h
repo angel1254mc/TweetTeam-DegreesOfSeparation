9eaab65c014b2aff9a1b4db3aefe8c20cff3fd35
angel1254mc <angellopezpol1254@gmail.com>
2021-12-08 03:04:24 -0500

    Committing code so I can access it on desktop

--- src/dataParse.h
+++ src/dataParse.h
@@ -13,6 +13,7 @@ using namespace std;
 struct dataParse {
     static void loadNetwork(ifstream* dataStream, EdgeList* graph);
     static void loadNetwork(ifstream* dataStream, AdjList* graph);
+    static void loadNetworkDual(ifstream* dataStream, AdjList* AdjGraph, EdgeList* EdgeGraph);
 };
 
 /**
@@ -25,7 +26,7 @@ struct dataParse {
 void dataParse::loadNetwork(ifstream* dataStream, EdgeList* graph)
 {
     int weight = 1; // This weight changes depending on the dataset. In this case, its just 1.
-    vector<pair<string, string>> EdgeList;
+    //vector<pair<string, string>> EdgeList;
     string parse = "not empty";
     string temp;
     while (parse != "")
@@ -35,18 +36,19 @@ void dataParse::loadNetwork(ifstream* dataStream, EdgeList* graph)
         getline(*dataStream, parse, '\n');
         if (parse == "")
             break;
-        EdgeList.push_back(pair<string,string>(temp, parse));
+        //EdgeList.push_back(pair<string,string>(temp, parse));
+        graph->insertEdge(stoi(temp), stoi(parse), weight);
     }
-    for (auto pair : EdgeList)
+    /**for (auto pair : EdgeList)
     {
         graph->insertEdge(stoi(pair.first), stoi(pair.second), weight);
-    }
+    }*/
 }
 
 void dataParse::loadNetwork(ifstream* dataStream, AdjList* graph)
 {
     int weight = 1; // This weight changes depending on the dataset. In this case, its just 1.
-    vector<pair<string, string>> EdgeList;
+    //vector<pair<string, string>> EdgeList;
     string parse = "not empty";
     string temp;
     while (parse != "")
@@ -56,10 +58,38 @@ void dataParse::loadNetwork(ifstream* dataStream, AdjList* graph)
         getline(*dataStream, parse, '\n');
         if (parse == "")
             break;
-        EdgeList.push_back(pair<string,string>(temp, parse));
+        //EdgeList.push_back(pair<string,string>(temp, parse));
+        graph->insertEdge(stoi(temp), stoi(parse), weight);
     }
-    for (auto pair : EdgeList)
+    /**for (auto pair : EdgeList)
     {
         graph->insertEdge(stoi(pair.first), stoi(pair.second), weight);
+    }*/
+}
+
+/**
+ * Takes in a read stream for a dataset and two pointer to two different graph object. Parses dataset and stores it 
+ * in the graph objects
+ * @param dataStream denotes the pointer to the ifstream
+ * @param AdjGraph denotes a pointer to the adjacency list graph object.
+ * @param EdgeGraph denotes a pointer to the edge list
+ * @note ifstream needs to have opened file already open
+ */
+void dataParse::loadNetworkDual(ifstream* dataStream, AdjList* AdjGraph, EdgeList* EdgeGraph)
+{
+    int weight = 1; // This weight changes depending on the dataset. In this case, its just 1.
+    //vector<pair<string, string>> EdgeList;
+    string parse = "not empty";
+    string temp;
+    while (parse != "")
+    {
+        getline(*dataStream, parse, ' ');
+        temp = parse;
+        getline(*dataStream, parse, '\n');
+        if (parse == "")
+            break;
+        //EdgeList.push_back(pair<string,string>(temp, parse));
+        AdjGraph->insertEdge(stoi(temp), stoi(parse), weight);
+        EdgeGraph->insertEdge(stoi(temp),stoi(parse),weight);
     }
 }
